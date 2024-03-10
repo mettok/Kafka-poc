e76923d208871843fc09edb0fe9c6fe120a58877
mettok <mehdi@zare.no>
2024-03-10 18:19:11 +0100

    docker-compose.yaml

--- Kafka-message-consumer/Dockerize.sh
+++ Kafka-message-consumer/Dockerize.sh
@@ -1,4 +1,4 @@
 mvn clean install
-docker build -t kafka-message-delivery .
+docker build -t kafka-message-consumer .
 #docker tag k8shello-backend mehdiz/k8shello-backend:1.0.0
 #docker push mehdiz/k8shello-backend:1.0.0
